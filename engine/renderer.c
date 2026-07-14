77f85c5b64fb073b33a6bddbf2a316e2662e506a
ayanchavand <ayan.chavand15@gmail.com>
2026-07-15 03:06:56 +0530

    refact: names and appData implementation

--- engine/renderer.c
+++ engine/renderer.c
@@ -6,18 +6,18 @@
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
-bool Dry_RendererInit(const char *title, int width, int height){
+bool Dry_RendererInit(const Dry_AppInfo* app, int width, int height){
 	
 
-	if(!SDL_CreateWindowAndRenderer(title, width ,height, 0, &window, &renderer)){
-        	SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create windoiw and renderer: %s", SDL_GetError());
+	if(!SDL_CreateWindowAndRenderer(app->appname, width ,height, 0, &window, &renderer)){
+        	SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
         	return false;
 	}
 	return true;
 	
 }
 
-bool Dry_RendererInitDefault(const char *title){
-	return Dry_RendererInit(title, DRY_DEFAULT_WINDOW_WIDTH, DRY_DEFAULT_WINDOW_HEIGHT);
+bool Dry_RendererInitDefault(const Dry_AppInfo* app){
+	return Dry_RendererInit(app, DRY_DEFAULT_WINDOW_WIDTH, DRY_DEFAULT_WINDOW_HEIGHT);
 }
 
--- engine/renderer.h
+++ engine/renderer.h
@@ -1,14 +1,20 @@
-#ifndef RENDERER_H
-#define RENDERER_H
+#ifndef DRY_RENDERER_H
+#define DRY_RENDERER_H
 
 #include <stdbool.h>
 
 #define DRY_DEFAULT_WINDOW_WIDTH 640
 #define DRY_DEFAULT_WINDOW_HEIGHT 480
 
-bool Dry_RendererInit(const char* title, int width, int height);
+typedef struct {
+	const char* appname;
+	const char* appversion;
+	const char* appidentifier;
+} Dry_AppInfo;
 
-bool Dry_RendererInitDefault(const char* title);
+bool Dry_RendererInit(const Dry_AppInfo* app , int width, int height);
+
+bool Dry_RendererInitDefault(const Dry_AppInfo* app);
 
 void Dry_RendererShutdown(void);
 
