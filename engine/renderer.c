f6879b4512179bc084cbd2b260498a25cbe49d6d
ayanchavand <ayan.chavand15@gmail.com>
2026-07-17 01:06:31 +0530

    fix: image loading

--- engine/renderer.c
+++ engine/renderer.c
@@ -6,19 +6,20 @@
 static SDL_Window* window = NULL;
 static SDL_Renderer* renderer = NULL;
 static SDL_Texture* tex = NULL;
-SDL_FRect defult_rect = {100,100,10,10};
+SDL_FRect default_rect = {100,100,256,256};
 //Default image test texture
 
  
 bool Dry_RendererInit(const Dry_AppInfo* app, int width, int height){
 	
-
+	
 	if(!SDL_CreateWindowAndRenderer(app->appname, width ,height, 0, &window, &renderer)){
         	SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
         	return false;
 	}
+
+	tex = load_my_image(renderer, "res/img/square.png");		
 	return true;
-	
 }
 
 bool Dry_RendererInitDefault(const Dry_AppInfo* app){
@@ -31,7 +32,7 @@ void Dry_RendererShutdown(){
 }
 
 void Dry_BeginFrame(){
-   	tex = load_my_image(renderer, "res/img/square.png");
+   	//tex = load_my_image(renderer, "res/img/square.png");		
 	SDL_SetRenderDrawColor(renderer, 128, 128, 128, 255); 
 	SDL_RenderClear(renderer);
 }
@@ -43,8 +44,10 @@ void Dry_EndFrame(){
 	Dry_Color red = {0, 0, 255, 255};
 	//Dry_DrawRect(100, 100, 100, 100, &red);
 	//Dry_DrawRect(300, 300, 20, 50, &red);
- 	SDL_RenderTexture(renderer, tex, NULL, NULL);
-	//Final Draw
+ 	if (!SDL_RenderTexture(renderer, tex, NULL, &default_rect))
+{
+    SDL_Log("Render failed: %s", SDL_GetError());
+}	//Final Draw
 	SDL_RenderPresent(renderer);
 }
 void Dry_DrawRect(const int w, const int h, const int x, const int y, const Dry_Color* color){
