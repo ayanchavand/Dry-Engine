c9322d3b45988a9ffa5efa275a8b2e7f2ea7d76b
ayanchavand <ayan.chavand15@gmail.com>
2026-07-18 00:24:41 +0530

    refact: cleanup for main

--- main.c
+++ main.c
@@ -1,78 +1,82 @@
-//TODO: Just bring up a window running
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_error.h>
-#include <SDL3/SDL_events.h>
-#include <SDL3/SDL_init.h>
-#include <SDL3/SDL_log.h>
-#include "engine/renderer.h"
+
 #include "core/time.h"
+#include "engine/renderer.h"
 
-int main (int argc, char *argv[]){
-	/*
-	const char* appname = "myGame";
-	const char* appversion = "0.01";
-	const char* appidentifier = "com.ayan.game";
-	*/
-	
-	Dry_AppInfo app_data = { "myGame","0.01","com.ayan.game"};
-	 
-	bool done = false;
-
-	int check = SDL_Init(SDL_INIT_EVENTS | SDL_INIT_VIDEO | SDL_INIT_AUDIO);
-	
-	SDL_SetAppMetadata(app_data.appname
-		    , app_data.appversion
-		    , app_data.appidentifier
-		    );
-	
-	Dry_RendererInitDefault(&app_data);
-	Dry_TimeInit();
-	//Seperate init for Debug purposes
-	/*	
-	window = SDL_CreateWindow(appname, SCREEN_W, SCREEN_H, 0);
- 	if (window == NULL) {
-        	SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
-        	SDL_Quit();
-        	return 1;
-    	}
-
-	
-	renderer = SDL_CreateRenderer(window, NULL);
-	if (renderer == NULL) {
-		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create renderer: %s\n", SDL_GetError());
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-		return 1;
-	}
-	*/
-	
-	
-
-	while(!done){
-		
-		Dry_TimeUpdate();
-
-		float dt = Dry_GetDeltaTime();
-
-if (dt > 0.0f)
+int main(int argc, char *argv[])
 {
-    SDL_Log("FPS: %.2f", 1.0f / dt);
-}
-		SDL_Event event;
-		while(SDL_PollEvent(&event)){
-			if(event.type == SDL_EVENT_QUIT){
-				done = true;
-			}
-		}
-		
-		//Background
-		Dry_BeginFrame();
-		Dry_EndFrame();
+    (void)argc;
+    (void)argv;
 
-}
-	
-	
-	Dry_RendererShutdown();
-	SDL_Quit();
-	return 0;
+    Dry_AppInfo app = {
+        .appname = "myGame",
+        .appversion = "0.01",
+        .appidentifier = "com.ayan.game"
+    };
+
+    SDL_SetAppMetadata(
+        app.appname,
+        app.appversion,
+        app.appidentifier
+    );
+
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_AUDIO))
+    {
+        SDL_LogError(
+            SDL_LOG_CATEGORY_ERROR,
+            "SDL_Init failed: %s",
+            SDL_GetError()
+        );
+        return 1;
+    }
+
+    Dry_RendererInitDefault(&app);
+    Dry_TimeInit();
+
+    bool running = true;
+
+    float fpsTimer = 0.0f;
+
+    while (running)
+    {
+        Dry_TimeUpdate();
+
+        // Log FPS once per second
+        fpsTimer += Dry_GetDeltaTime();
+        if (fpsTimer >= 1.0f)
+        {
+            SDL_Log("FPS: %.2f", 1.0f / Dry_GetDeltaTime());
+            fpsTimer = 0.0f;
+        }
+
+        // Handle events
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_EVENT_QUIT:
+                    running = false;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        // Update
+        // TODO: Game logic
+
+        // Render
+        Dry_BeginFrame();
+
+        // TODO: Draw calls
+
+        Dry_EndFrame();
+    }
+
+    Dry_RendererShutdown();
+    SDL_Quit();
+
+    return 0;
 }
